e5153e24d50f80e5987e8e973d1cb33bd32aa294
gabrielalegrete <gabsantos@f3capital.com.br>
2022-10-29 13:34:52 -0300

    class 5

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,8 +1,12 @@
-describe('Beach forecast functional tests', () => {
-  it('Should return a forecast with just a few times', async() => {
-    const { body, status } = await global.testRequest.get('/forecast');
-    expect(status).toBe(200);
-    expect(body).toEqual([{
+import { Request, Response } from 'express';
+import { Controller, Get } from '@overnightjs/core';
+
+@Controller('forecast')
+export class ForecastController {
+
+  @Get('')
+  public getForecastForLoggedUser(_: Request, res: Response): void {
+    res.send([{
       "time": "2020-04-26T00:00:00+00:00",
       "forecast": [{
         "lat": -33.792726,
@@ -37,5 +41,5 @@ describe('Beach forecast functional tests', () => {
       "windSpeed": 100
     }]
     }])
-  })
-})
+  }
+}
---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import { Application } from 'express';
+import bodyParser from 'body-parser';
+import './util/module-alias'
+import { Server } from '@overnightjs/core';
+import { ForecastController } from './controllers/forecast';
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
+
+  public init(): void {
+    this.setupExpress();
+    this.setupController();
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
+
+  private setupController(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController])
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
+}
