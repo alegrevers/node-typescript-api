a4d44731cb31a7a9511daff62464f58517098236
gabrielalegrete <gabsantos@f3capital.com.br>
2022-10-29 21:55:52 -0300

    beaches test

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,8 +1,11 @@
+import { Beach } from "@src/models/beach";
+
 describe('Beaches function tests', () => {
+  beforeAll(async () => await Beach.deleteMany({}))
   describe('When creating a beach', () => {
     it('Should create a beach with success', async () => {
       const newBeach = {
-        let: -33.792726,
+        lat: -33.792726,
         lng: 151.289824,
         name: 'Manly',
         position: 'E',
@@ -12,5 +15,22 @@ describe('Beaches function tests', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
     });
+
+    it('Should return 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      }
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"'
+      })
+    })
   });
 });
