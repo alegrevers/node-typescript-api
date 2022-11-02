b3d198ccf8d24e94aa522e2f54fa65d2b254b10f
gabrielalegrete <gabsantos@f3capital.com.br>
2022-11-02 17:06:56 -0300

    class 23

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import { User } from '@src/models/user';
 import mongoose from "mongoose";
 import { BaseController } from "./index";
+import AuthService from "@src/services/auth";
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -15,5 +16,34 @@ export class UsersController extends BaseController {
     } catch (error: any) {
       this.sendCreateUpdateErrorResponse(res, error)
     }
+  }
+
+  @Post('authenticate')
+  public async authenticate(req: Request, res: Response): Promise<Response | undefined> {
+    const { email, password } = req.body
+    
+    // try {
+      const user = await User.findOne({ email })
+  
+      if(!user) {
+        return res.status(401).send({
+          code: 401,
+          error: 'User not found!',
+        })
+      }
+
+      if (!(await AuthService.comparePasswords(password, user.password))) {
+        return res.status(401).send({
+          code: 401,
+          error: 'Password does not match!',
+        })
+      }
+
+      const token = AuthService.generateToken(user.toJSON())
+
+      return res.status(200).send({ token: token });
+    // } catch (error: any) {
+    //   console.error(error)
+    // }
   }
 }
