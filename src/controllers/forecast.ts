96024772981742c028348b27a9d0ca9102cb8eb5
gabrielalegrete <gabsantos@f3capital.com.br>
2022-12-11 01:09:17 -0300

    class26

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -13,6 +13,7 @@ export class ForecastController {
   @Get('')
   public async getForecastForLoggedUser(req: Request, res: Response): Promise<void> {
     try {
+      logger.error("🚀 ~ file: forecast.ts:17 ~ req", req.decoded)
       const beaches = await Beach.find({ user: req.decoded?.id });
       const forecastData = await forecast.processForecastForBeaches(beaches);
       res.status(200).send(forecastData);
