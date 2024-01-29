a510fc490c5afa41644925aa2d4f707273cb2d49
alegrevers <gabrielalegrete1@gmail.com>
2024-01-29 20:36:31 -0300

    fix deploy

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,9 @@ import { ForecastController } from './controllers/forecast';
 import * as database from '@src/database';
 import { BeachesController } from './controllers/beaches';
 import { UsersController } from './controllers/users';
-import logger from './logger';
 import expressPino from 'express-pino-logger'
 import cors from 'cors'
+import logger from './logger';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -23,9 +23,7 @@ export class SetupServer extends Server {
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
-    this.app.use(expressPino({
-      logger
-    }));
+    this.app.use(expressPino({ logger }));
     this.app.use(cors({ origin: '*' }))
   }
 
