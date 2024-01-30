2a6d7d765a8fb95030f566d1423d4933c981622b
alegrevers <gabrielalegrete1@gmail.com>
2024-01-30 19:41:28 -0300

    finish rating

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from '@src/middlewares/auth';
 import { Beach } from '@src/models/beach';
 import { Request, Response } from 'express';
 import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
-import { Forecast } from '@src/services/forecast';
+import { BeachForecast, Forecast } from '@src/services/forecast';
 import { BaseController } from '.';
 
 const forecast = new Forecast();
@@ -13,8 +13,15 @@ export class ForecastController extends BaseController {
   @Get('')
   public async getForecastForLoggedUser(req: Request, res: Response): Promise<void> {
     try {
+      const {
+        orderBy,
+        orderField,
+      }: {
+        orderBy?: 'asc' | 'desc';
+        orderField?: keyof BeachForecast;
+      } = req.query;
       const beaches = await Beach.find({ user: req.decoded?.id });
-      const forecastData = await forecast.processForecastForBeaches(beaches);
+      const forecastData = await forecast.processForecastForBeaches(beaches, orderBy, orderField);
       res.status(200).send(forecastData);
     } catch (error) {
       this.sendErrorResponse(res, {code: 500, message: 'Somethin went wrong'})
