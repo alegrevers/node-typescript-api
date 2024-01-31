1632ae262188d7e487ab1d86d8c58ffed82cf9a6
alegrevers <gabrielalegrete1@gmail.com>
2024-01-31 17:16:02 -0300

    finish docs

--- web/src/config.js
+++ web/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://alegrevers.vercel.app/';
+export const API_URL = 'https://alegrevers.vercel.app';
 export const GMAPS_API_KEY = 'AIzaSyBB854Viw6N2qgak_TT-C91kHKky82POYw';
